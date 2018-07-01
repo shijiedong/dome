169bcbf7a1d5402d4b2484dd8eaca03c81d4106c
shane <usershane@163.com>
2018-07-01 14:17:04 +0800

    添加模型与数据迁移

--- shane/models.py
+++ shane/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class TaskMain(models.Model):
+	strName = models.CharField(max_length=10)
+	strType = models.CharField(max_length=10)
+
